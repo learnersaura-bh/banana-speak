c6bdf1603aa6e8aaca084b85f873fb34ffe088ad
learnersaura-bh <119805882+learnersaura-bh@users.noreply.github.com>
2022-12-20 23:39:03 +0530

    fix-error-handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var errorMessage = document.querySelector("#error-msg")
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
@@ -20,6 +21,11 @@ function clickHandler(){
             var translatedText = json.contents.translated;
         outputDiv.innerText = translatedText;
     })
+    .catch(errorHandler)
+}
+
+function errorHandler(){
+   errorMessage.innerText = "*Server is not working, try after sometime"
 }
 
 btnTranslate.addEventListener("click", clickHandler);
